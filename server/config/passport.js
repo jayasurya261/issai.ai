ae390d75274e25ca02533933fc0de56fc02affee
Surya <suryasunrise261@gmail.com>
2025-12-17 20:13:14 +0530

    patch work

--- server/config/passport.js
+++ server/config/passport.js
@@ -19,7 +19,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: process.env.CALLBACK_URL || '/auth/google/callback',
+            callbackURL: 'https://issai-ai.vercel.app/auth/google/callback',
             proxy: true
         },
         async (accessToken, refreshToken, profile, done) => {
